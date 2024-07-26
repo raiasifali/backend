174734ab3ec0f82e499800f069e4d1ec2e358dc1
raiasifali <asif.developer92@gmail.com>
2024-07-26 21:10:51 +0500

    Frontend origin add

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ const allowedOrigins = [
 ];
 // CORS configuration
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
